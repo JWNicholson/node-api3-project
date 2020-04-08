f936f9f5d128c90032bb66f890a566c0efa15dd1
JW Nicholson <john.nicholson812@gmail.com>
2020-04-08 15:37:32 -0400

    setting up routers and middleware on server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
-//routers
-
-
-//server.use functions
-
-//endpoints
+//router
+const userRouter = require('./users/userRouter');
 
 const server = express();
+
+//server.use functions
+server.use(express.json());
+server.use(logger);
+
+//endpoints
+server.use('./api/users', userRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -15,6 +19,14 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.url);
+  console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get(
+        "Origin"
+      )}`
+  );
+  next();
+}
 
 module.exports = server;
