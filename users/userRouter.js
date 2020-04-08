049a2f0323cadbe6d96ad576bc173445d28910f0
JW Nicholson <john.nicholson812@gmail.com>
2020-04-08 17:28:59 -0400

    needs troubleshooting

--- users/userRouter.js
+++ users/userRouter.js
@@ -56,16 +56,53 @@ router.get('/:id',validateUserId, (req, res) => {
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  const id = req.params.id;
+
+  userDB.getUserPosts(id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+      .catch(err => {
+        res.status(500).json({message: "Could not retrieve any posts for that user."});
+      });
 });
 //////////////////////////////////////////
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const id = req.params.id;
+
+  userDB.remove(id)
+    .then(deletedUser => {
+      if(deletedUser === 1) {
+        res.status(200).json({message: `User with ID of ${id} is now gone forever.`})
+      }else{
+        res.status(500).json({message: "Error - could not delete user."})
+      }
+    })
+      res.status(500).json({message:"Server could not delete user." })
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',validateUser, validateUserId, (req, res) => {
   // do your magic!
+  const id = req.params.id
+  
+  userDB.update(id, req.body)
+  .then(updateUser => {
+    userDB.getById(id)
+        .then(user => {
+          if (updateUser === 1){ 
+            res.status(200).json(user);
+          } else {
+            res.status(500).json({ message: "Could not update user."});
+            }
+          })
+            .catch(err => {
+            res.status(500).json({ message: "Error while trying to modify the user in the database.", err})
+          });
+    })
 });
 
 //custom middleware
