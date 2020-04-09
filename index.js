578b73e2828930aa70f32b78bc51e401e07c90d1
JW Nicholson <john.nicholson812@gmail.com>
2020-04-09 16:16:57 -0400

    environment varible for port added

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ require("dotenv").config(); // reads values from .env file and merges them into 
 
 const server = require('./server.js');
 
-const port = process.env.PORT || 7000;
 //const port = 7000;
-
+const port = process.env.PORT || 7000;
 server.listen(port, () => {
     console.log(`\n=== server running on port ${port}\n ===`);
 });
