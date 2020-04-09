dd94b912732f14daba03a758085bd6f988666dd0
JW Nicholson <john.nicholson812@gmail.com>
2020-04-09 15:51:15 -0400

    npm start test passed. installed dotenv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// code away!
+require("dotenv").config(); // reads values from .env file and merges them into process.env
+
 const server = require('./server.js');
 
-const port = 7000;
+const port = process.env.PORT || 7000;
+//const port = 7000;
 
 server.listen(port, () => {
     console.log(`\n=== server running on port ${port}\n ===`);
